5f58064d93727d32ceff0c7a98825a7afc674ba9
syamozipc <syamozipc@gmail.com>
2022-01-02 14:42:19 +0900

    update

--- app/models/Post.php
+++ app/models/Post.php
@@ -19,29 +19,34 @@ class Post {
 
     public function save($post, $filePath)
     {
-        $this->db->beginTransaction();
+        try {
+            $this->db->beginTransaction();
 
-        $sql = 'INSERT INTO posts (user_id, country_id, description) VALUES (:user_id, :country_id, :description)';
-        $this->db->prepare($sql);
+            $sql = 'INSERT INTO posts (user_id, country_id, description) VALUES (:user_id, :country_id, :description)';
+            $this->db->prepare($sql);
 
-        $this->db->bind(':user_id', 1);
-        $this->db->bind(':country_id', $post['country_id']);
-        $this->db->bind(':description', $post['description']);
+            $this->db->bind(':user_id', 1);
+            $this->db->bind(':country_id', $post['country_id']);
+            $this->db->bind(':description', $post['description']);
 
-        $this->db->execute();
+            $this->db->execute();
 
-        $id = $this->db->lastInsertId();
+            $id = $this->db->lastInsertId();
 
-        $sql = 'INSERT INTO post_details (post_id, type, path, sort_number) VALUES (:post_id, :type, :path, :sort_number)';
-        $this->db->prepare($sql);
+            $sql = 'INSERT INTO post_details (post_id, type, path, sort_number) VALUES (:post_id, :type, :path, :sort_number)';
+            $this->db->prepare($sql);
 
-        $this->db->bind(':post_id', $id);
-        $this->db->bind(':type', 1);
-        $this->db->bind(':path', basename($filePath));
-        $this->db->bind(':sort_number', 1);
+            $this->db->bind(':post_id', $id);
+            $this->db->bind(':type', 1);
+            $this->db->bind(':path', basename($filePath));
+            $this->db->bind(':sort_number', 1);
 
-        $this->db->execute(':description', $post['description']);
+            $this->db->execute(':description', $post['description']);
 
-        $this->db->commit();
+            $this->db->commit();
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            echo $e->getMessage();
+        }
     }
 }
