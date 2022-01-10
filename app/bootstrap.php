3bac9b5dbcf7e087611cd093f16ac4c0273fbc0d
syamozipc <syamozipc@gmail.com>
2022-01-10 11:39:24 +0900

    config.phpをrename、helperファイルを追加

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 // 使い回す定数のfile
-require_once 'config/config.php';
+require_once 'config/app.php';
+require_once 'helpers/app.php';
 
 // autoloader
 // 未定義のclassが呼ばれた時に引数のcallbackが実行される。callbackの引数にはclass名が入る
