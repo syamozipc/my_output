04bf5aa13aa0749c8f362df927d088fb9e1068c0
syamozipc <syamozipc@gmail.com>
2022-02-03 22:18:26 +0900

    webpackのentryをリファクタ

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,24 @@
 // Node.jsに組み込まれているモジュール。出力先などの指定をするために利用する。
 const path = require('path');
 
-// scssのコンパイルに使用
+// scssのcompileに使用
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// compile対象の全JS file
+const entry = {
+	'user/home/index': './resources/js/user/home/index.js',
+	'user/post/index': './resources/js/user/post/index.js',
+	'user/post/confirm': './resources/js/user/post/confirm.js',
+	'user/post/create': './resources/js/user/post/create.js',
+	'user/post/edit': './resources/js/user/post/edit.js',
+	'user/post/editConfirm': './resources/js/user/post/editConfirm.js',
+	'user/post/show': './resources/js/user/post/show.js',
+}
 
 // modeは各環境用webpack config fileに記述しているので、ここでは共通処理のみ
 module.exports = {
-	entry: {
-		'user/home/index': './resources/js/user/home/index.js',
-		'user/post/index': './resources/js/user/post/index.js',
-		'user/post/confirm': './resources/js/user/post/confirm.js',
-		'user/post/create': './resources/js/user/post/create.js',
-		'user/post/edit': './resources/js/user/post/edit.js',
-		'user/post/editConfirm': './resources/js/user/post/editConfirm.js',
-		'user/post/show': './resources/js/user/post/show.js',
-	},
+	// entry: entry の省略
+	entry,
 	output: {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'js/[name].js',
