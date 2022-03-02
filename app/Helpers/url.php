fbdcb7e2d7e3fdce2362e489522a9c1a304b414f
syamozipc <syamozipc@gmail.com>
2022-03-02 09:48:49 +0900

    todoコメントを対応

--- app/Helpers/url.php
+++ app/Helpers/url.php
@@ -20,8 +20,6 @@ function redirect($route, $replace = true, $status = 302)
 /**
  * ページ遷移用url生成処理
  * 
- * @todo /が来たらtopページへのURLになるようにする
- *
  * @param string $route 遷移先パス
  * @param string|array|null $params 任意のパラメータ
  * @return string ページ遷移用url
